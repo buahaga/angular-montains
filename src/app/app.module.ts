1ea855ddfa87d859594ab6c16dfe73859bef28be
andrey <buahaga@ya.ru>
2018-05-15 15:51:03 +0300

    components setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { ListComponent } from './list/list.component';
+import { ListItemComponent } from './list-item/list-item.component';
+import { MapComponent } from './map/map.component';
+import { SortComponent } from './sort/sort.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ListComponent,
+    ListItemComponent,
+    MapComponent,
+    SortComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
